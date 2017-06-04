861d786f62a5e7f943369a6337d99cb9d5ef6e00
rombod <rambodmagid@yahoo.com>
2017-06-03 18:31:09 -0600

    second

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+class Product < ApplicationRecord
+	has_many :orders
+	has_many :comments
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :colour, presence: true
+  validates :price, presence: true
+  validates :image_url, presence: true
+  
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
+
+	def average_rating
+  comments.average(:rating).to_f
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :users
+  resources :orders, only: [:index, :show, :create, :destroy]
+  resources :products do
+    resources :comments
+  end
+
+  root 'products#index'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/index'
+  get 'static_pages/landing_page'
+  post 'static_pages/thank_you'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
