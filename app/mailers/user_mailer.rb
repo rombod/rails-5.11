1b27200ef31ac23ba0bcd7c787508c74d3cbb4e8
rombod <rambodmagid@yahoo.com>
2017-06-29 13:45:12 -0600

    2gstwr

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,11 +7,11 @@ def contact_form(email, name, message)
 mail(:from => email, :to => 'mahmood.madgidi@farauto.ca', :subject => "A new contact form message from #{name}")   
 end 
 
-def welcome(user)
-  	@user = user
-  	@appname = "rails-5.11"
-  	mail(to: user.email, subject: "Welcome to #{@appname}!")
-	end
+#def welcome(user)
+  	#@user = user
+  	#@appname = "rails-5.11"
+  	#mail(to: user.email, subject: "Welcome to #{@appname}!")
+	#end
 
 def thank_you
     @name = params[:name]
@@ -23,3 +23,13 @@ end
 
   
 
+
+
+
+
+  #trying to do thing through the controller instead
+  def new_registrations(user)
+   @user = user
+    mail(:to => @user.email, 
+        :subject => "New User created please review and enable.")
+  end
